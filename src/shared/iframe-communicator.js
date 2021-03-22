abaab4749e3335aef7da5d17e028c70d89cc1390
Josep Viciana <emmgfx@gmail.com>
2021-03-22 16:19:34 +0100

    Reverse condition

--- src/shared/iframe-communicator.js
+++ src/shared/iframe-communicator.js
@@ -1,12 +1,12 @@
 const isSSR = typeof window === 'undefined';
 
 const sendMessageToVideo = message => {
-  if(!isSSR) return;
+  if(isSSR) return;
   window.top.frames['video-frame'].postMessage(message, "*");
 }
 
 const sendMessageToChat = message => {
-  if(!isSSR) return;
+  if(isSSR) return;
   window.top.frames['chat-frame'].postMessage(message, "*");
 }
 
