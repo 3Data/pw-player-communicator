cf9be842e7f755d6a56373849bf45a220a7b4aa8
Jose Moreno <jose.moreno@3data.net>
2021-07-21 15:55:31 +0200

    Added tester Example

---
+++ examples/tester/tester.js
@@ -0,0 +1,135 @@
+// Config:
+
+var sessionToken = document.querySelector("#sessionToken").textContent;
+var communicator = new PWPlayer.Communicator({sessionToken});
+
+// UI:
+
+var btGoPrivate = document.querySelector("#btGoPrivate");
+var btRequestVIP = document.querySelector("#btRequestVIP");
+var btDestroyVideo = document.querySelector("#btDestroyVideo");
+var btVol50 = document.querySelector("#btVol50");
+var btVol100 = document.querySelector("#btVol100");
+var btMute = document.querySelector("#btMute");
+var btUnmute = document.querySelector("#btUnmute");
+var btActivateCam = document.querySelector("#btActivateCam");
+var btDeactivateCam = document.querySelector("#btDeactivateCam");
+var btSendTip100 = document.querySelector("#btSendTip100");
+var btSendToy50 = document.querySelector("#btSendToy50");
+
+var formChat = document.querySelector("#chat-form");
+var input = formChat.querySelector("input");
+var chatMessages = document.querySelector("#chat-messages");
+var roomModeIndicator = document.querySelector("#roomMode");
+var VIPRequestStatusIndicator = document.querySelector("#VIPRequestStatus");
+
+// Video events:
+
+communicator.on("videoPlay", function(){
+  console.log("video play in tester");
+});
+
+communicator.on("videoStop", function(){
+  console.log("video stop in tester");
+});
+
+communicator.on("videoOffline", function(){
+  console.log("video offline in tester");
+});
+
+communicator.on("videoError", function(){
+  console.log("video error in tester");
+});
+
+communicator.on("disconnected", function(reason){
+  console.log("disconnected in tester", reason);
+});
+
+// Chat events:
+
+communicator.on("chatMessage", function(data){
+  console.log("chat message in tester:", data);
+  var node = document.createElement("li");
+  var textNode = document.createTextNode(JSON.stringify(data));
+  node.appendChild(textNode);
+  chatMessages.appendChild(node);
+});
+
+communicator.on("roomModeUpdate", function(roomMode){
+  console.log("roomModeUpdate on tester", roomMode);
+  roomModeIndicator.textContent = roomMode;
+  updateFormAvailability(roomMode);
+});
+
+communicator.on("VIPRequestStatusUpdate", function(status){
+  console.log("VIPRequestStatusUpdate on tester", status);
+  VIPRequestStatusIndicator.textContent = status;
+  updateVIPButtonAvailability(status);
+});
+
+// Actions
+
+btDestroyVideo.addEventListener("click", function(){
+  communicator.destroyVideo();
+});
+
+btGoGroupChat.addEventListener("click", function(){
+  communicator.goToGroupChat();
+});
+
+btRequestVIP.addEventListener("click", function(){
+  communicator.requestVIP();
+});
+
+btVol50.addEventListener("click", function(){
+  communicator.setVolume(0.2);
+});
+
+btVol100.addEventListener("click", function(){
+  communicator.setVolume(1);
+});
+
+btMute.addEventListener("click", function(){
+  communicator.setMuted(true);
+});
+
+btUnmute.addEventListener("click", function(){
+  communicator.setMuted(false);
+});
+
+btActivateCam.addEventListener("click", function(){
+  communicator.activateUserCam();
+});
+
+btDeactivateCam.addEventListener("click", function(){
+  communicator.deactivateUserCam();
+});
+
+btSendTip100.addEventListener("click", function(){
+  communicator.sendTip(100);
+});
+
+btSendToy50.addEventListener("click", function(){
+  communicator.sendToy(50);
+});
+
+formChat.addEventListener("submit", function(e){
+  e.preventDefault();
+  if(input.value === "") return;
+  communicator.sendMessage(input.value);
+  input.value = "";
+})
+
+function updateFormAvailability(roomMode){
+  var disabled = !['freechat', 'private', 'vip'].includes(roomMode);
+  Array.from(formChat.children).forEach(function(children){
+    children.disabled = disabled;
+  });
+}
+
+function updateVIPButtonAvailability(status){
+  btRequestVIP.disabled = ['requesting', 'waiting', 'accepted'].includes(status);
+}
+
+updateFormAvailability();
+updateVIPButtonAvailability();
---
+++ examples/tester/tester.php
@@ -0,0 +1,105 @@
+<?php
+define('SECRET', "YOUR_SECRET_API_KEY");
+define('PARTNER_ID',0);
+define('APP_ID', 0);
+define('URL_API',"YOUR_API_URL");
+?>
+<html>
+  <head>
+    <title>🛠️ TBR: ROOM TESTER</title>
+    <link rel="stylesheet" href="./tester.css"></link>
+  </head>
+  <body>
+    <?php
+    $iframe = getIframe();
+    ?>
+    <div id="sessionToken"><?php echo $iframe->sessionToken; ?></div>
+    <div>RoomMode: <span id="roomMode"></span></div>
+    <div>VIPRequest status: <span id="VIPRequestStatus"></span></div>
+    <div id="iframes-container">
+      <iframe name="video-frame" src="<?php echo $iframe->urlVideo; ?>" style="width:100%; height:400px;"  allow="camera;microphone"></iframe>
+      <iframe name="chat-frame" src="<?php echo $iframe->urlChat; ?>" style="width:100%; height:400px;"></iframe>
+      <ul id="chat-messages"></ul>
+    </div>
+    <button id="btDestroyVideo">Destruir video</button>
+    <button id="btVol50">Volumen al 20%</button>
+    <button id="btVol100">Volumen al 100%</button>
+    <button id="btMute">Mute</button>
+    <button id="btUnmute">Unmute</button>
+    <button id="btActivateCam">Activar cámara</button>
+    <button id="btDeactivateCam">Desactivar cámara</button>
+    <hr />
+    <button id="btGoGroupChat">Ir a Group Chat</button>
+    <button id="btRequestVIP">Solicitar VIP</button>
+    <hr />
+    <button id="btSendTip100">Send tip 100</button>
+    <button id="btSendToy50">Send toy 50</button>
+    <hr />
+    <form id="chat-form">
+      <input type="text" />
+      <button type="submit">Enviar</button>
+    </form>
+
+    <hr />
+
+    <?php
+    echo '<pre>';
+    print_r($iframe);
+    echo '</pre>';
+    ?>
+    <script src="https://unpkg.com/@pw2016/pw-player-communicator@^1.9.2/dist/pw-room-handler.js"></script>
+    <script src="./tester.js"></script>
+  </body>
+</html>
+
+<?php
+function doCurl($endpoint, $body){
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_POST, 1);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
+
+  // OPTIONS:
+  curl_setopt($curl, CURLOPT_URL, $endpoint);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+  // EXECUTE:
+  $result = curl_exec($curl);
+  curl_close($curl);
+
+  return json_decode($result);
+}
+
+function getIframe(){
+  $data = [
+    # Required:
+    "method"        => "getIframe",
+    "roomMode"      => "auto",
+    "userToken"     => isset($_GET['userToken']) ? $_GET['userToken'] : "usertoken",
+    "userNick"      => isset($_GET['userNick']) ? $_GET['userNick'] : "userNick",
+    "appId"         => APP_ID,
+    "modelId"       => isset($_GET['modelId']) ? $_GET['modelId'] : 00000,
+    "userId"        => isset($_GET['userId']) ? $_GET['userId'] : "userid",
+    "partner_token" => getPartnerToken(),
+    "pruebas"       => 1,
+    "trackId"       => "test",
+  ];
+  return doCurl(URL_API."brokers.php", $data)->message;
+}
+
+function getPartnerToken(){
+
+  $cacheFileName = "partnerToken.dat";
+
+  if(file_exists($cacheFileName) && time() - filemtime($cacheFileName) < 86400){ // 24 hours
+    $partnerToken = file_get_contents($cacheFileName);
+  }else{
+    $partnerToken = doCurl(URL_API."partnerauth.php", [
+      "secret"      => SECRET,
+      "partner_id"  => PARTNER_ID,
+    ])->message->partner_token;
+    file_put_contents($cacheFileName, $partnerToken);
+  }
+
+  return $partnerToken;
+}
+?>
